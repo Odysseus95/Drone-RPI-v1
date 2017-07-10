6b736a4016cd65eb153e4eb2e446c71150e7d01f
Odysseus95 <marius.trimbur@gmail.com>
2017-07-10 15:59:18 +0200

    pause

---
+++ RPI/src/libstream/pistream.c
@@ -0,0 +1,17 @@
+/*-----------------------------
+This library provides functions to capture video with the picamera
+and to stream it with gstreamer.
+To use it, you should create a process wich is responsible of the video capture, and a process responsible of the streaming. 
+The communication between the two proccess should be handled with a pipe
+
+Author : Odysseus
+------------------------------*/ 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
